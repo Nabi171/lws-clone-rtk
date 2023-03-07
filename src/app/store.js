d2c25601955ebfe2e885417786d79481806ec49b
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-03-07 16:38:25 +0600

    changedkl

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import videosReducer from '../features/videos/videosSlice';
 import videoReducer from '../features/video/videoSlice';
 import tagsReducer from '../features/tags/tagsSlice';
-
+import relatedVideosReuducer from "../features/relatedVideos/relatedVideosSlice"
 export const store = configureStore({
   reducer: {
     videos: videosReducer,
     tags: tagsReducer,
     video: videoReducer,
+    relatedVideos: relatedVideosReuducer
   },
 });
--- src/components/list/RelatedVideoList.js
+++ src/components/list/RelatedVideoList.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import RelatedVideo from './RelatedVideoListItem';
 import RelatedVideoListItem from './RelatedVideoListItem';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRelatedVideos } from '../../features/relatedVideos/relatedVideosSlice';
+import Loading from '../ui/Loading';
 
-const RelatedVideoList = () => {
+const RelatedVideoList = ({ currentVideoId, tags }) => {
+    const dispatch = useDispatch();
+    const { relatedVideos, isLoading, isError, error } = useSelector(((state) => state.relatedVideos))
+    useEffect(() => {
+        dispatch(fetchRelatedVideos({ tags, id: currentVideoId }))
+    }, [dispatch, tags, currentVideoId]);
+
+    //decide what to render
+    let content = null;
+    if (isLoading) {
+        content = <Loading />
+    }
+    if (!isLoading && isError) {
+        content = <div className="col-span-12">{error}</div>;
+    }
+    if (!isLoading && !isError && relatedVideos ?.length === 0) {
+        content = <div className="col-span-12">No related videos found!</div>;
+    }
+    if (!isLoading && !isError && relatedVideos ?.length > 0) {
+        content = relatedVideos.map((video) => (
+            <RelatedVideoListItem key={video.id} video={video} />
+        ));
+    }
+
     return (
-        <div>
-            <div
-                class="col-span-full lg:col-auto max-h-[570px] overflow-y-auto"
-            >
-                {/* <!-- single related video --> */}
-                <div class="w-full flex flex-row gap-2 mb-4">
-                    <RelatedVideoListItem></RelatedVideoListItem>
-                </div>
-            </div>
-
+        <div class="col-span-full lg:col-auto max-h-[570px] overflow-y-auto">
+            {content}
         </div>
     );
 };
